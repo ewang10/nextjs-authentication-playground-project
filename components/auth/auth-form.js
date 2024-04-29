93b1f0a74271f09390c8d795a5b6a2b57e79e2da
eric <ericwang27759@gmail.com>
2024-04-28 18:23:31 -0700

    added route protection to profile and auth pages

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import classes from './auth-form.module.css';
 
 async function createUser(email, password) {
@@ -24,6 +25,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -43,6 +45,10 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword
       });
+
+      if (!result.error) {
+        router.replace('/profile');
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,30 @@
+import { getSession } from 'next-auth/react';
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  // getSession will automatically looks into the request
+  // and extract the data it needs, the session token cookie,
+  // and see if it's valid
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false  // indicate if it's permanent or temporary redirect
+      }
+    };
+  }
+
+  return {
+    props: {
+      session
+    }
+  };
 }
 
 export default ProfilePage;
